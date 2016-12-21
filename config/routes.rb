eeef0c319905688eba3e77c63d65fc810a8ccc48
barnameh <farimahd@gmail.com>
2016-12-21 16:59:40 -0500

    Added fitbi_auth controller, omniauth.rb and omniauth_fitbit gem. Modified routes to incorporate fitbit authorization.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  # root 'welcome#index'
+
+  # Provides a button to launch auth
+    root "fitbit_auth#index"
+
+  # fitbit oauth
+  post "/auth/fitbit" => "fitbit_auth#make_request"
+  get "/auth/fitbit/callback" => "fitbit_auth#get_response"
 end
